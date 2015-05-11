5ab7f1bc9950b995a2f22e81182059d6c5a81dd3
ojolao <ojolao@gmail.com>
2015-05-11 14:45:22 -0700

    coded guess button behavior

--- js/app.js
+++ js/app.js
@@ -12,6 +12,41 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
+  	var compGuess = Math.floor((Math.random() * 100) + 1);
+  	
+
+  	
+
+  	
+
+	$("input#guessButton").click(function(e){
+		e.preventDefault();
+		var userGuess1 = $("input#userGuess").val();
+		var guessDifference = absDifference(compGuess, userGuess1);
+		function absDifference (a, b) {
+  		return Math.abs(a - b);
+  	};
+  		console.log(compGuess);
+  		console.log(userGuess1);
+  		if (guessDifference === 0){
+  			$("#feedback").html("You guessed exactly right and won the game!");
+  		} else if (guessDifference <= 20){
+  			$("#feedback").html("you are hot! Keep trying" );
+  		} else if (guessDifference > 20){
+  			$("#feedback").html("you are cold");
+  		};
+
+  		$("#guessList").prepend('<li>' + userGuess1 + '</li>');
+  		  	})
+
+  	/*---Start a new game---*/
+  	$("a.new").click(function(){
+
+  		
+  	});
+
+
 });
 
 
